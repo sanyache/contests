6f57e1a6fab1093d3a84e57c1e01eacc0aae5e8d
sanyache <sanyache75@gmail.com>
2025-03-16 12:04:53 +0200

    longest segment

---
+++ two_pointers/longest_segment.py
@@ -0,0 +1,16 @@
+"""
+sliding window визначити довжину найдовшого відрізку,
+сума елементів якого не первищує s
+"""
+s = int(input())
+nums = [int(x) for x in input().split()]
+l = 0
+cur_sum = 0
+max_l = 0
+for r in range(len(nums)):
+    cur_sum += nums[r]
+    while cur_sum > s:
+        cur_sum -= nums[l]
+        l += 1
+    max_l = max(max_l, (r-l+1))
+print(max_l)

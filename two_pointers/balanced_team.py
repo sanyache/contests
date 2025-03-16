c3b1bdb3b3f76c5d318762ee5e4494342b98c444
sanyache <sanyache75@gmail.com>
2025-03-16 21:39:03 +0200

    balances team

---
+++ two_pointers/balanced_team.py
@@ -0,0 +1,14 @@
+"""
+Знайти максимальну кількість членів команди,
+показник скілів яких не перевищує k
+"""
+k = int(input())
+team = [int(x) for x in input().split()]
+team.sort()
+max_k = 0
+l =0
+for r in range(1, len(team)):
+    while team[r] - team[l] > k:
+        l += 1
+    max_k = max(max_k, r-l+1)
+print(max_k)

abad89cc4cf9f61a17dd1722495f37cfa646165c
sanyache <sanyache75@gmail.com>
2025-03-16 12:54:02 +0200

    longest substring

---
+++ two_pointers/longest_substring.py
@@ -0,0 +1,11 @@
+s = input()
+l = 0
+s_map = set()
+max_l = 0
+for r in range(len(s)):
+    while s[r] in s_map:
+        s_map.remove(s[l])
+        l += 1
+    s_map.add(s[r])
+    max_l = max(max_l, len(s_map))
+print(max_l)

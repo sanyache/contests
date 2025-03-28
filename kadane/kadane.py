fbf5f983bb5e864cbfc5bda26d6cb251c834bff4
sanyache <sanyache75@gmail.com>
2025-03-28 20:02:35 +0200

    kadane

---
+++ kadane/kadane.py
@@ -0,0 +1,14 @@
+"""
+Знайти максимальну суму підмасиву
+-1  2 4 -3 5 2 -5 2
+10
+
+"""
+numbers = [int(x) for x in input().split()]
+n = len(numbers)
+best = 0
+curr_sum = 0
+for i in range(n):
+    curr_sum = max(curr_sum+numbers[i], numbers[i])
+    best = max(best, curr_sum)
+print(best)
---
+++ kadane/kadane_naitive.py
@@ -0,0 +1,15 @@
+"""
+Знайти максимальну суму підмасиву
+-1  2 4 -3 5 2 -5 2
+10
+
+"""
+numbers = [int(x) for x in input().split()]
+n = len(numbers)
+best = 0
+for l in range(n):
+    curr_sum = 0
+    for r in range(l, n):
+        curr_sum += numbers[r]
+        best = max(best, curr_sum)
+print(best)

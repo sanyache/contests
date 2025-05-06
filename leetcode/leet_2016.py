211dc8553e087c5945763d2c1eb62227808df2ff
sanyache <sanyache75@gmail.com>
2025-05-06 18:20:28 +0300

    leet_2016

---
+++ leetcode/leet_2016.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumDifference(self, a: List[int]) -> int:
+        n = len(a)
+        stack = list()
+        stack.append(a[-1])
+        best = 0
+        for i in range(n-2, -1, -1):
+            while stack and a[i] >= stack[-1]:
+                stack.pop()
+            stack.append(a[i])
+            if len(stack) > 1:
+                best = max((stack[0] - stack[-1]), best)
+
+        return best if best else -1

28f54bbaf55cd14f47e8f03fb3aede7127f457f1
oleksandrchemerys <sanyache75@gmail.com>
2025-12-19 17:40:22 +0200

    leet 1422+

--- leetcode/leet_1422.py
+++ leetcode/leet_1422.py
@@ -1,14 +1,17 @@
 class Solution:
     def maxScore(self, s: str) -> int:
         n = len(s)
-        prefix_sum = [0]*(n + 1)
-        prefix_sum[n-1] = int(s[n-1])
-        for i in range(n-1, -1, -1):
-            prefix_sum[i] = int(s[i]) + prefix_sum[i+1]
+        cnt_1 = 0
+        for i in range(1, n):
+            if s[i] == '1':
+                cnt_1 += 1
         cnt_0 = 1 if s[0] == '0' else 0
-        max_sc = cnt_0 + prefix_sum[1]
-        for i in range(1, n-1):
+        best = cnt_0 + cnt_1
+        for i in range(1, n - 1):
             if s[i] == '0':
                 cnt_0 += 1
-            max_sc = max(max_sc, cnt_0 + prefix_sum[i+1])
-        return max_sc
+            else:
+                cnt_1 -= 1
+            best = max(best, cnt_0 + cnt_1)
+
+        return best
